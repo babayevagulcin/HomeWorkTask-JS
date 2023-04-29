4092759ea0a7e73b75e78332e07fe7bb1e6c7885
[babayevagulcin] <[7ebv570@code.edu.az]>
2023-04-30 01:42:26 +0400

    Calculator

--- 20.Dom.Calculator/script.js
+++ 20.Dom.Calculator/script.js
@@ -8,24 +8,42 @@ let divisionBtn = document.querySelector(".division-btn");
 let resetBtn = document.querySelector(".reset-btn");
 
 let resultValue;
-additionBtn.addEventListener('click',()=>{
-     resultValue = +value1Input.value + +value2Input.value;
-    resultInput.innerText = resultValue;
-})
-substractBtn.addEventListener("click",function(){
+additionBtn.addEventListener("click", () => {
+  resultValue = +value1Input.value + +value2Input.value;
+  resultInput.innerText = resultValue;
+  if (value1Input.value === "" || value2Input.value === "") {
+    alert("Enter number");
+  }
+});
+substractBtn.addEventListener("click", function () {
     resultValue = +value1Input.value - +value2Input.value;
     resultInput.innerText = resultValue;
-})
-multiplyBtn.addEventListener("click",function(){
-    resultValue = +value1Input.value * +value2Input.value;
-    resultInput.innerText = resultValue;
-})
-divisionBtn.addEventListener("click",function(){
-    resultValue = +value1Input.value / +value2Input.value;
-    resultInput.innerText = resultValue;
-})
-resetBtn.addEventListener("click",function(){
-    value1Input.value='';
-    value2Input.value='';
+    if (value1Input.value === "" || value2Input.value === "") {
+      alert("Enter number");
+    }
+});
+multiplyBtn.addEventListener("click", function () {
+  resultValue = +value1Input.value * +value2Input.value;
+  resultInput.innerText = resultValue;
+  if (value1Input.value === "" || value2Input.value === "") {
+    alert("Enter number");
+  }
+});
+divisionBtn.addEventListener("click", function () {
+  resultValue = +value1Input.value / +value2Input.value;
+  resultInput.innerText = resultValue;
+  if (value1Input.value === "" || value2Input.value === "") {
+    alert("Enter number")
+  }
+  if (value2Input.value === "0") {
     resultInput.innerText = "0";
-})
+  }
+});
+resetBtn.addEventListener("click", function () {
+  value1Input.value = "";
+  value2Input.value = "";
+  resultInput.innerText = "0";
+  if (value1Input.value === "" || value2Input.value === "") {
+    alert("Enter number");
+  }
+});
