6808a5cdc0f9d6c2617d2f467d6b61d5c344a061
[babayevagulcin] <[7ebv570@code.edu.az]>
2023-05-15 00:12:01 +0400

    loopstudios

---
+++ 22.LoopStudios/script.js
@@ -0,0 +1,6 @@
+let menuIcon = document.querySelector(".menu-icon");
+let navToogle = document.querySelector(".toggle");
+
+menuIcon.addEventListener("click", function () {
+  navToogle.classList.toggle("navbar-toogle");
+});
